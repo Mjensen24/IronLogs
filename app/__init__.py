fce67dbdb3024aa41178ee1513319ece7466e051
Michael Jensen <mcjensen24@gmail.com>
2021-02-17 17:23:10 -0500

    deleted dockerfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, send_from_directory
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
@@ -16,7 +16,8 @@ from .seeds import seed_commands
 
 from .config import Config
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder="../react-app/build/static")
 
 # Setup login manager
 login = LoginManager(app)
@@ -69,11 +70,18 @@ def inject_csrf_token(response):
                         httponly=True)
     return response
 
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
     print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
-    return app.send_static_file('index.html')
+    if path == 'favicon-32x32.png':
+        return send_from_directory('../react-app/build/', "favicon-32x32.png")
+    return send_from_directory('../react-app/build/', 'index.html')
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def react_root(path):
+#     print("path", path)
+#     if path == 'favicon.ico':
+#         return app.send_static_file('favicon.ico')
+#     return app.send_static_file('index.html')
