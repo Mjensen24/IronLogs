c3270858e4ed72d031d25980917455e60f7d7461
Michael Jensen <mcjensen24@gmail.com>
2021-02-19 17:11:55 -0500

    deleted /build in the git ignore

---
+++ react-app/build/precache-manifest.0e37096b46459cbb18ff350f3f93863a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5604f775f1c10e9d5e8fde536c6491ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7695d7b4a51bce70f6c",
+    "url": "/static/css/main.f0c95d64.chunk.css"
+  },
+  {
+    "revision": "763d760ee2d31be86732",
+    "url": "/static/js/2.523273de.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.523273de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7695d7b4a51bce70f6c",
+    "url": "/static/js/main.75cd9ae0.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
