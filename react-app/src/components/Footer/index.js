c702f65591f6a2bd298e2b063e6885ea13befef1
Michael Jensen <mcjensen24@gmail.com>
2021-03-31 19:33:58 -0400

    working on footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.css'
+import { Redirect } from "react-router-dom";
 import { CgProfile } from 'react-icons/cg';
 import { FiGithub, FiLinkedin, FiMail, } from 'react-icons/fi';
 
@@ -11,14 +12,16 @@ const Footer = () => {
             <div className="bottom">
                 <div className="footer-info">
                     <div className="footer-icons">
-                        <CgProfile className="profile" />
+                        <CgProfile className="profile">
+                            <Redirect to="https://mjensen24.github.io/" />
+                        </CgProfile>
                         <FiLinkedin className="linkedin" />
                         <FiMail className="mail" />
                         <FiGithub className="github" />
                     </div>
                     <div className="footer-legal">
                         <p className="white">Developed by Michael Jensen</p>
-                        <p className="gray">2021 Iron Logs</p>
+                        <p className="gray">© 2021 Iron Logs</p>
                     </div>
                 </div>
             </div>
@@ -28,7 +31,7 @@ const Footer = () => {
 
 export default Footer;
 
-{/* <SocialIcon url="https://github.com/Mjensen24" />
-                <SocialIcon url="https://www.linkedin.com/in/michael-jensen-636901169/" />
-                <SocialIcon url="http://twitter.com/jaketrent" />
-                <SocialIcon url="https://mcjensen24@gmail.com" /> */}
+// <SocialIcon url="https://github.com/Mjensen24" />
+// <SocialIcon url="https://www.linkedin.com/in/michael-jensen-636901169/" />
+//<SocialIcon url="http://twitter.com/jaketrent" />
+//<SocialIcon url="https://mcjensen24@gmail.com" />
