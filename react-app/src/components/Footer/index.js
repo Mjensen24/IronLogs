15ff8acc04fb97540267288a5e66821a28975da9
Michael Jensen <mcjensen24@gmail.com>
2021-03-31 17:59:33 -0400

    working on updating footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.css'
-import { SocialIcon } from 'react-social-icons';
+import { CgProfile } from 'react-icons/cg';
+import { FiGithub, FiLinkedin, FiMail, } from 'react-icons/fi';
 
 const Footer = () => {
     return (
@@ -8,13 +9,26 @@ const Footer = () => {
             <div className="top">
             </div>
             <div className="bottom">
-                <SocialIcon url="https://github.com/Mjensen24" />
-                <SocialIcon url="https://www.linkedin.com/in/michael-jensen-636901169/" />
-                <SocialIcon url="http://twitter.com/jaketrent" />
-                <SocialIcon url="https://mcjensen24@gmail.com" />
+                <div className="footer-info">
+                    <div className="footer-icons">
+                        <CgProfile className="profile" />
+                        <FiLinkedin className="linkedin" />
+                        <FiMail className="mail" />
+                        <FiGithub className="github" />
+                    </div>
+                    <div className="footer-legal">
+                        <p className="white">Developed by Michael Jensen</p>
+                        <p className="gray">2021 Iron Logs</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Footer;
+
+{/* <SocialIcon url="https://github.com/Mjensen24" />
+                <SocialIcon url="https://www.linkedin.com/in/michael-jensen-636901169/" />
+                <SocialIcon url="http://twitter.com/jaketrent" />
+                <SocialIcon url="https://mcjensen24@gmail.com" /> */}
