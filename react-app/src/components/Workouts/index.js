6797da372f4f0656112c0dd8f8c71f8595f2df19
Michael Jensen <mcjensen24@gmail.com>
2021-05-12 18:00:07 -0400

    heroku

--- react-app/src/components/Workouts/index.js
+++ react-app/src/components/Workouts/index.js
@@ -35,6 +35,7 @@ const Workouts = ({ userId }) => {
             </div >
             <div className="post-button">
                 <WorkoutDrawer setWorkouts={setWorkouts} userId={userId} />
+                <h1>test</h1>
             </div>
         </>
     )
